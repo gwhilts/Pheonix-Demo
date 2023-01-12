5f3370e510171966426797e0d0a4ad2776b8259c
Gerald Hilts <gwhilts@booknowl.com>
2023-01-11 17:09:39 -0800

    Implement pub sub
    Add pub sub functionality to the Posts live view so that
    changes are immediately visiable across all clients.

--- lib/chirp_web/live/post_live/index.ex
+++ lib/chirp_web/live/post_live/index.ex
@@ -6,6 +6,7 @@ defmodule ChirpWeb.PostLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
+    if connected?(socket), do: Timeline.subscribe()
     {:ok, assign(socket, :posts, list_posts())}
   end
 
@@ -38,6 +39,11 @@ defmodule ChirpWeb.PostLive.Index do
     {:ok, _} = Timeline.delete_post(post)
 
     {:noreply, assign(socket, :posts, list_posts())}
+  end
+
+  @impl true
+  def handle_info({:post_created, post}, socket) do
+    {:noreply, update(socket, :posts, fn posts -> [post | posts] end)}
   end
 
   defp list_posts do
