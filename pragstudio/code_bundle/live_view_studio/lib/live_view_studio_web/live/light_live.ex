a1f239e0f2082c72bf789237878a313f8fb71f7f
Gerald Hilts <gwhilts@booknowl.com>
2023-02-07 17:19:16 -0800

    End of Lesson 3, "Button Clicks"
    Implements the Front Porch Light live view
    with buttons to adjust the light slider.

---
+++ pragstudio/code_bundle/live_view_studio/lib/live_view_studio_web/live/light_live.ex
@@ -0,0 +1,48 @@
+defmodule LiveViewStudioWeb.LightLive do
+  use LiveViewStudioWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, brightness: 10)}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1>Front Porch Light</h1>
+    <div id="light">
+      <div class="meter">
+        <span style={"width: #{@brightness}%"}><%=@brightness %>%</span>
+      </div>
+      <button phx-click="off"><img src="/images/light-off.svg"/></button>
+      <button phx-click="down"><img src="/images/down.svg"/></button>
+      <button phx-click="up"><img src="/images/up.svg"/></button>
+      <button phx-click="on"><img src="/images/light-on.svg"/></button>
+    </div>
+    """
+  end
+
+  def handle_event("on", _, socket) do
+    socket =
+      assign(socket,
+        brightness: 100
+      )
+      {:noreply, socket}
+  end
+
+  def handle_event("off", _, socket) do
+    socket =
+      assign(socket,
+        brightness: 0
+      )
+      {:noreply, socket}
+  end
+
+  def handle_event("down", _, socket) do
+    {:noreply, update(socket, :brightness, &(max 0, &1 - 10))}
+  end
+
+  def handle_event("up", _, socket) do
+    {:noreply, update(socket, :brightness, &(min &1 + 10, 100))}
+  end
+
+
+end
--- pragstudio/code_bundle/live_view_studio/lib/live_view_studio_web/router.ex
+++ pragstudio/code_bundle/live_view_studio/lib/live_view_studio_web/router.ex
@@ -19,6 +19,7 @@ defmodule LiveViewStudioWeb.Router do
 
     get "/", PageController, :home
 
+    live "/light", LightLive
     live "/sandbox", SandboxLive
     live "/sales", SalesLive
     live "/flights", FlightsLive
