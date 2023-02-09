60591335b994f4f046d46d5e31f1b0aeda63e054
Gerald Hilts <gwhilts@booknowl.com>
2023-02-08 16:53:00 -0800

    Add dimmer slider and temp control to LightLive (end Ch. 5 notes)

--- pragstudio/code_bundle/live_view_studio/lib/live_view_studio_web/live/light_live.ex
+++ pragstudio/code_bundle/live_view_studio/lib/live_view_studio_web/live/light_live.ex
@@ -2,7 +2,7 @@ defmodule LiveViewStudioWeb.LightLive do
   use LiveViewStudioWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, brightness: 10)}
+    {:ok, assign(socket, brightness: 10, temp: "3000")}
   end
 
   def render(assigns) do
@@ -10,8 +10,21 @@ defmodule LiveViewStudioWeb.LightLive do
     <h1>Front Porch Light</h1>
     <div id="light">
       <div class="meter">
-        <span style={"width: #{@brightness}%"}><%=@brightness %>%</span>
+        <span style={"width: #{@brightness}%; background: #{temp_color(@temp)}"}><%=@brightness %>%</span>
       </div>
+      <form phx-change="dim">
+        <input type="range" min="0" max="100" name="brightness" value={@brightness}/>
+      </form>
+      <br/>
+      <form phx-change="update_temp">
+        <div class="temps">
+          <%= for temp <- ["3000", "4000", "5000"] do %>
+            <input type="radio" id={temp} name="temp" value={temp} checked={temp == @temp}/>
+            <label for={temp}><%= temp %>°</label>
+          <% end %>
+        </div>
+      </form>
+      <br/>
       <button phx-click="off"><img src="/images/light-off.svg"/></button>
       <button phx-click="down"><img src="/images/down.svg"/></button>
       <button phx-click="up"><img src="/images/up.svg"/></button>
@@ -40,4 +53,17 @@ defmodule LiveViewStudioWeb.LightLive do
   def handle_event("random", _, socket) do
     {:noreply, assign(socket, brightness: Enum.random(0..100))}
   end
+
+  def handle_event("dim", %{"brightness" => b}, socket) do
+    {:noreply, assign(socket, brightness: b)}
+  end
+
+  def handle_event("update_temp", %{"temp" => temp}, socket) do
+    {:noreply, assign(socket, temp: temp)}
+  end
+
+
+  defp temp_color("3000"), do: "#F1C40D"
+  defp temp_color("4000"), do: "#F3FF66"
+  defp temp_color("5000"), do: "#99CCFF"
 end
