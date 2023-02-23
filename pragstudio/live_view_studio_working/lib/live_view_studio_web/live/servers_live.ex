bbdcfabf131a7434dee217dc95cc845c11f5b5ce
Gerald Hilts <gwhilts@booknowl.com>
2023-02-23 15:13:06 -0800

    pragstudio: Extract Server function comp. (end Ch. 11 notes)

--- pragstudio/live_view_studio_working/lib/live_view_studio_web/live/servers_live.ex
+++ pragstudio/live_view_studio_working/lib/live_view_studio_web/live/servers_live.ex
@@ -48,37 +48,43 @@ defmodule LiveViewStudioWeb.ServersLive do
       </div>
       <div class="main">
         <div class="wrapper">
-          <div class="server">
-            <div class="header">
-              <h2><%= @selected_server.name %></h2>
-              <span class={@selected_server.status}>
-                <%= @selected_server.status %>
-              </span>
-            </div>
-            <div class="body">
-              <div class="row">
-                <span>
-                  <%= @selected_server.deploy_count %> deploys
-                </span>
-                <span>
-                  <%= @selected_server.size %> MB
-                </span>
-                <span>
-                  <%= @selected_server.framework %>
-                </span>
-              </div>
-              <h3>Last Commit Message:</h3>
-              <blockquote>
-                <%= @selected_server.last_commit_message %>
-              </blockquote>
-            </div>
-          </div>
+          <.server server={@selected_server} />
           <div class="links">
             <.link navigate={~p"/light"}>
               Adjust lights
             </.link>
           </div>
         </div>
+      </div>
+    </div>
+    """
+  end
+
+  def server(assigns) do
+    ~H"""
+    <div class="server">
+      <div class="header">
+        <h2><%= @server.name %></h2>
+        <span class={@server.status}>
+          <%= @server.status %>
+        </span>
+      </div>
+      <div class="body">
+        <div class="row">
+          <span>
+            <%= @server.deploy_count %> deploys
+          </span>
+          <span>
+            <%= @server.size %> MB
+          </span>
+          <span>
+            <%= @server.framework %>
+          </span>
+        </div>
+        <h3>Last Commit Message:</h3>
+        <blockquote>
+          <%= @server.last_commit_message %>
+        </blockquote>
       </div>
     </div>
     """
