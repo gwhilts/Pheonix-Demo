0d068fbf775d90ae0489a4ccc975c74584f4026d
Gerald Hilts <gwhilts@booknowl.com>
2023-02-24 10:46:21 -0800

    pragstudio: Add sorting to Donations table (end Ch. 12 vid)

--- pragstudio/live_view_studio_working/lib/live_view_studio_web/live/donations_live.ex
+++ pragstudio/live_view_studio_working/lib/live_view_studio_web/live/donations_live.ex
@@ -5,12 +5,38 @@ defmodule LiveViewStudioWeb.DonationsLive do
 
   def mount(_params, _session, socket) do
     donations = Donations.list_donations()
-
-    socket =
-      assign(socket,
-        donations: donations
-      )
-
     {:ok, socket, temporary_assigns: [donations: []]}
+  end
+
+  def handle_params(params, _uri, socket) do
+    sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
+    options = %{
+      sort_by: sort_by,
+      sort_order: sort_order
+    }
+
+    socket = assign(socket,
+     donations: Donations.list_donations(options),
+     options: options
+    )
+
+    {:noreply, socket}
+  end
+
+  def sort_link(assigns) do
+    ~H"""
+    <.link patch={~p"/donations?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"}>
+      <%= render_slot(@inner_block) %>
+    </.link>
+    """
+  end
+
+  defp next_sort_order(sort_order) do
+    case sort_order do
+      :asc -> :desc
+      :desc -> :asc
+    end
   end
 end
